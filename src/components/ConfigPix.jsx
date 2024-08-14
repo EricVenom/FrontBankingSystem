5e81b61ca60443783392a8aa6dc54f449feec7a6
Eric Soares <horrorhaus666@gmail.com>
2024-08-14 16:48:54 -0300

    chaves pix aparecem e desaparecem agora

--- src/components/ConfigPix.jsx
+++ src/components/ConfigPix.jsx
@@ -4,11 +4,13 @@ import { Button } from '@mui/material';
 import { getCookie } from '../utils/storage';
 import api from '../services/api'
 import DeleteIcon from '@mui/icons-material/Delete';
+import LinearIndeterminate from './LinearIndeterminate';
 import "./ConfigPix.css"
 
 export default function ConfigPix({ setConfigPix }) {
     const { loggedUser, loggedBankAccounts, loggedPixKeys, setLoggedPixKeys } = useContext(loggedUserContext);
     const enumArray = ["CPF", "CNPJ", "EMAIL", "TELEPHONE"];
+    const [loading, setLoading] = useState(false);
 
     const [selectedKey, setSelectedKey] = useState('');
     const [inputValue, setInputValue] = useState('');
@@ -50,20 +52,26 @@ export default function ConfigPix({ setConfigPix }) {
 
     const deletePixKey = async (id) => {
         try {
+            setLoading(true);
             await api.delete(`/pix-key/${id}`, {
                 headers: {
                     Authorization: `Bearer ${getCookie("auth")}`
                 }
-            })
+            });
+
+            setLoggedPixKeys(loggedPixKeys.filter(key => key.id !== id));
+
         } catch (error) {
             console.log(error);
+        } finally {
+            setLoading(false);
         }
     }
 
     const addPixKey = async () => {
         try {
-            setNewKey({ ...newKey, keyType: selectedKey, keyValue: inputValue });
-            const { accountId, keyType, keyValue } = newKey;
+            setLoading(true);
+            const { accountId, keyType, keyValue } = { ...newKey, keyType: selectedKey, keyValue: inputValue };
 
             if (accountId && keyType && keyValue) {
                 const { data: key } = await api.post(`/pix-key/account/${accountId}`, {
@@ -73,13 +81,15 @@ export default function ConfigPix({ setConfigPix }) {
                     headers: {
                         Authorization: `Bearer ${getCookie("auth")}`
                     }
-                })
+                });
 
-                setLoggedPixKeys({ ...loggedPixKeys, key })
+                setLoggedPixKeys([...loggedPixKeys, key]);
             }
 
         } catch (error) {
             console.log("addPixKey(): ", error);
+        } finally {
+            setLoading(false);
         }
     }
 
@@ -130,9 +140,9 @@ export default function ConfigPix({ setConfigPix }) {
 
                         <Button variant="contained" onClick={addPixKey}>Adicionar</Button>
                         <Button variant="text" onClick={() => setConfigPix(false)}>Cancelar</Button>
+                        {loading && <LinearIndeterminate />}
                     </form>
                 </section>
-
             </fieldset>
         </div>
     )
